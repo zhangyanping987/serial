889c23d2fb6bf05b34beccb07793b8d0de2cdf65
zhangyanping987 <zhangyanping987@outlook.com>
2025-04-27 19:23:49 +0800

    更新 serial_ros2 子模块内容

---
+++ build/serial/CMakeFiles/serial.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/zhang/myenv/bot/src/serial_ros2/src/impl/list_ports/list_ports_linux.cc" "CMakeFiles/serial.dir/src/impl/list_ports/list_ports_linux.cc.o" "gcc" "CMakeFiles/serial.dir/src/impl/list_ports/list_ports_linux.cc.o.d"
+  "/home/zhang/myenv/bot/src/serial_ros2/src/impl/unix.cc" "CMakeFiles/serial.dir/src/impl/unix.cc.o" "gcc" "CMakeFiles/serial.dir/src/impl/unix.cc.o.d"
+  "/home/zhang/myenv/bot/src/serial_ros2/src/serial.cc" "CMakeFiles/serial.dir/src/serial.cc.o" "gcc" "CMakeFiles/serial.dir/src/serial.cc.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/serial/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/zhang/myenv/bot/src/serial_ros2
+# Build directory: /home/zhang/myenv/bot/src/serial_ros2/build/serial
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
